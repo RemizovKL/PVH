75151167124d28d202a83a5c1ceef3f3570049ee
Kirill <kremizov3@gmail.com>
2025-11-14 14:10:20 +0300

    fixup! feat(db/InitializeDBData.py): Initialize db data

--- db/InitializeDB.py
+++ db/InitializeDB.py
@@ -1,27 +1,28 @@
 import sqlite3
-import uuid
+import json
 from loguru import logger as log
 
 def init_database():
     conn = sqlite3.connect('products.db')
     cursor = conn.cursor()
-    
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Category (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        atributes TEXT NOT NULL
+    )
+    ''')
+
     cursor.execute('''
     CREATE TABLE IF NOT EXISTS Product (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        description TEXT,
-        price REAL,
-        image_path TEXT NOT NULL
-    )
-    ''')
-    
-    cursor.execute('''
-    CREATE TABLE IF NOT EXISTS Type (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        product_id INTEGER,
-        FOREIGN KEY (product_id) REFERENCES Product (id)
+        category INTEGER NOT NULL,
+        desc TEXT,
+        image_path TEXT,
+        size TEXT,
+        FOREIGN KEY (category) REFERENCES Category (id)
     )
     ''')
     
